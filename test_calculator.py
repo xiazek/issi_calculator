98609ecf34c0a3bc0e4db2bea815ed1980d7994d
Maciej Książek <xiazek@users.noreply.github.com>
2025-11-03 20:59:57 +0100

    Configure pylint github action (#2)
    * Configure pylint github action
    
    * add pylint to requirements-dev.txtĻ
    
    * correct typo in word subtract
    
    * docstrings for test class + disable test method docsrting requirement in test file

--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,21 @@
+# skipping test method level docsrting requirement (test methods are self-documenting in most cases)
+# pylint: disable=C0116
+
+"""Unit tests for the `Calculator` class."""
 from unittest import TestCase
 from calculator import Calculator
 
 
 class TestCalculator(TestCase):
+    """Test suite for verifying `Calculator` arithmetic operations."""
 
     def test_calculator_sum(self):
         calc = Calculator(5, 3)
         assert calc.sum() == 8
 
-    def test_calculator_substract(self):
+    def test_calculator_subtract(self):
         calc = Calculator(5, 3)
-        assert calc.substract() == 2
+        assert calc.subtract() == 2
 
     def test_calculator_multiply(self):
         calc = Calculator(5, 3)
@@ -21,6 +26,6 @@ class TestCalculator(TestCase):
         assert calc.divide() == 1.6666666666666667
 
     def test_calculator_divide_by_zero(self):
+        """Calculator.divide should return None when dividing by zero."""
         calc = Calculator(5, 0)
         self.assertIsNone(calc.divide())
-
